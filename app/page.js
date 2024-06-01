23e60baeee8f1e8611181097e6497cb7f9c4a02d
Pipi022 <Oskar.michalik06@gmail.com>
2024-06-01 19:48:18 +0200

    updated refreshing

--- app/page.js
+++ app/page.js
@@ -3,8 +3,19 @@ import Form from "./components/home/form/Form";
 import Main from "./components/home/main/Main";
 import { Provider } from "react-redux";
 import store from "@/store/indexRedux";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname == "/search-tickets") {
+      window.location.replace("https://search-flights-project.web.app/");
+    }
+  }, [pathname, router]);
   return (
     <Provider store={store} key={"home"}>
       <Form />
